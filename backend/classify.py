9c16d5b3bcfe7dffe3f49ba2eba98df207832399
Illya Maznitskiy <comercaleuros@gmail.com>
2025-03-28 13:34:37 +0200

    Add more logs in classify.py

--- backend/classify.py
+++ backend/classify.py
@@ -1,10 +1,32 @@
+import os
+
 import tensorflow as tf
 import numpy as np
 import logging
 
+# Path to the model
 model_path = "model/model.h5"
+
+# Set up logging
 logging.basicConfig(level=logging.DEBUG)
 
+# Get the current directory
+current_directory = os.getcwd()
+logging.info(f"Current Directory: {current_directory}")
+
+# Log all files in the current directory
+files_in_directory = os.listdir(current_directory)
+logging.info(f"Files in Current Directory: {files_in_directory}")
+
+# Check if the model file exists
+if os.path.exists(model_path):
+    logging.info(f"Model file found at: {model_path}")
+else:
+    logging.error(f"Model file NOT found at: {model_path}")
+    # You can print the absolute path here as well
+    logging.error(f"Absolute model path: {os.path.abspath(model_path)}")
+
+# Attempt to load the model
 try:
     model = tf.keras.models.load_model(model_path)
     logging.info(f"Model loaded successfully from {model_path}")
