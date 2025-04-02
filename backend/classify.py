94d7a302c82930f2253f9d469f3cc665ea21d192
Illya Maznitskiy <comercaleuros@gmail.com>
2025-04-02 13:33:17 +0300

    Refactor classify.py for better consistency

--- backend/classify.py
+++ backend/classify.py
@@ -6,44 +6,28 @@ import numpy as np
 import logging
 
 
-# Path to the model
-MODEL_PATH = os.path.join(os.getcwd(), "model", "model.h5")
-MODEL_URL = "https://drive.google.com/uc?export=download&id=10Pg-LLFNIq6Gx_wHOQsauTe-2E-rGCcN"
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
-print("Model path:", os.path.abspath(MODEL_PATH))
+MODEL_PATH = os.path.join(os.getcwd(), "model", "model.h5")
+MODEL_URL = (
+    "https://drive.google.com/uc?export=download&"
+    "id=10Pg-LLFNIq6Gx_wHOQsauTe-2E-rGCcN"
+)
+
 
 # Ensure model exists and is correct size
 if (
     not os.path.exists(MODEL_PATH) or os.path.getsize(MODEL_PATH) < 1000000
 ):  # Less than 1MB = corrupt
-    print("\U0001f817 Model file is missing or corrupted. Downloading...")
+    logging.warning(
+        "\U0001f817 Model file " "is missing or corrupted. Downloading..."
+    )
     os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
     response = requests.get(MODEL_URL)
     with open(MODEL_PATH, "wb") as f:
         f.write(response.content)
-    print("✅ Model downloaded successfully!")
-
-
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-
-# Get the current directory
-current_directory = os.getcwd()
-logging.info(f"📂 Current Directory: {current_directory}")
-
-# Log all files in the directory
-files_in_directory = os.listdir(current_directory)
-logging.info(f"📄 Files in Current Directory: {files_in_directory}")
-
-# Check if the model file exists
-if os.path.exists(MODEL_PATH):
-    logging.info(f"✅ Model file found at: {MODEL_PATH}")
-    logging.info(f"📏 Model file size: {os.path.getsize(MODEL_PATH)} bytes")
-else:
-    logging.error(f"❌ Model file NOT found at: {MODEL_PATH}")
-    logging.error(f"🛠️ Absolute Path: {os.path.abspath(MODEL_PATH)}")
-    model = None  # Prevent further errors
-    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+    logging.info("✅ Model downloaded successfully!")
 
 # Attempt to load the model
 try:
@@ -55,7 +39,6 @@ except Exception as e:
     model = None  # Prevent crashes
 
 
-# Function to classify image
 def classify_image(img):
     if model is None:
         logging.error("🚨 Model is not loaded! Classification cannot proceed.")
@@ -69,7 +52,7 @@ def classify_image(img):
     class_idx = np.argmax(predictions[0])
     confidence = predictions[0][class_idx]
 
-    class_names = ["cats", "dogs"]  # Update to your model's classes
+    class_names = ["cats", "dogs"]
     predicted_class = class_names[class_idx]
 
     return predicted_class, confidence
